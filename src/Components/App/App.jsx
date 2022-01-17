80c0d4d7cf3f4994393bdb6e615ef95bc9f4eaab
Maxim <developement.future@gmail.com>
2022-01-18 00:38:00 +0100

    comment Footer

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from '../Header/Header'
 import Section from '../Section/Section'
 import Gallery from '../Gallery/Gallery'
+import Footer from '../Footer/Footer'
 
 const App = () => {
     return ( 
@@ -10,6 +11,7 @@ const App = () => {
            <div>{Header()}</div>
            <div>{Section()}</div>
            <div>{Gallery()}</div>
+            {/* <div>{Footer()}</div> */}
         </div>
     )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import '../Footer/Footer.css'
+
+
+const Footer = () => {
+    return ( 
+        <>
+            <div className = "Footer" >
+                <div className  = "Logo">
+                    <span>K</span>
+                    <img src="../LogoHouse.png" ></img>
+                    <span>sa</span>
+                </div>
+                <p className='c'>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
