c983d03092a725f9ba26b83a5af6c7db87775065
Maxim <developement.future@gmail.com>
2022-01-17 17:42:30 +0100

    amélioration css section

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,10 +1,14 @@
 import './Section.css'
+import IMG from '../../IMG.png'
 
 const Section = () => {
+
     return ( 
 
         <div className = "Section" >
-            <div className='phot-container'></div>
+            <div className = "SectionFilter"></div>
+            <div className='phot-container' style={{backgroundImage: `url(${IMG})`}}></div>
+            <h1 className='SectionTxt'>Chez vous, partout et ailleurs</h1>
         </div>
     )
 }
--- src/Components/Thumb/Thumb.jsx
+++ src/Components/Thumb/Thumb.jsx
@@ -1,13 +1,15 @@
 import '../Thumb/Thumb.css'
+import DATA from '../../DATA'
 
 
 const Thumb = () => {
+    const bgImg = DATA[0].cover
     return ( 
         <>
-            <div className = "Thumb" >
+            <div className = "Thumb" style={{backgroundImage: `url(${bgImg})`}}>
                 <a href="thumb-link">
-                    <div className='Thumb-filter'>
-                        <span className='thumb-title'>Titre de la location</span>
+                    <div className='Thumb-filter' >
+                        <span className='thumb-title'>{DATA[0].title}</span>
                     </div>
                 </a>
             </div>
