4db2bdff8ac8167e82d7eac8991b507cba8ac782
Maxim <developement.future@gmail.com>
2022-01-21 13:00:20 +0100

    page about avec dataAbout

--- src/Components/DropdownLarge.jsx
+++ src/Components/DropdownLarge.jsx
@@ -3,22 +3,16 @@ import ReactDom from "react-dom"
 import "../css/DropdownLarge.css"
 
 const DropdownLarge = (props) => {
-  let title = "Fiabilité"
   return (
     <div className="dropdown-Container">
       <div className="dropdown-head">
-        <h2 className="dropdown-title">{title}</h2>
+        <h2 className="dropdown-title">{props.title}</h2>
         <span className="dropdown-arrow">
-          <i class="fas fa-chevron-down"></i>
+          <i className="fas fa-chevron-down"></i>
         </span>
       </div>
       <div className="dropdown-body">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error
-          qui distinctio a excepturi, sunt illo quam cumque dignissimos numquam
-          minus odit iusto velit repellendus perspiciatis! Harum corrupti natus
-          ducimus.
-        </p>
+        <p>{props.txt}</p>
       </div>
     </div>
   )
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,12 +6,44 @@ import DropdownLarge from "../Components/DropdownLarge"
 // import DATA from "../DATA"
 
 const About = (props) => {
+  const dataAbout = [
+    {
+      title: "Fiabilité",
+      bodyText:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+    },
+    {
+      title: "Respect",
+      bodyText:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      bodyText:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      bodyText:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
+
   return (
     <div className="About">
       <Header />
-      <div className="about-cont">
-        <SectionAbout />
-        <DropdownLarge data={props} />
+      <SectionAbout />
+      <div className="dropdown-items">
+        {dataAbout.map((item) => {
+          console.log(item.title)
+          return (
+            <DropdownLarge
+              title={item.title}
+              txt={item.bodyText}
+              key={item.title}
+            />
+          )
+        })}
       </div>
     </div>
   )
