0eb1c896c0c9424f3363727cb1fefa7a32410ecb
Maxim <developement.future@gmail.com>
2022-01-23 20:02:13 +0100

    open close dropdownlarge ok

--- src/Components/DropdownLarge.jsx
+++ src/Components/DropdownLarge.jsx
@@ -1,21 +1,56 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React, { Component } from "react"
 import "../css/DropdownLarge.css"
 
-const DropdownLarge = (props) => {
-  return (
-    <div className="dropdown-Container">
-      <div className="dropdown-head">
-        <h2 className="dropdown-title">{props.title}</h2>
-        <span className="dropdown-arrow">
-          <i className="fas fa-chevron-down"></i>
-        </span>
+export default class DropdownLarge extends Component {
+  state = { openBody: false }
+
+  handleBodyDropdown = () => {
+    this.setState({ openBody: !this.state.openBody })
+  }
+
+  render() {
+    return (
+      <div className="dropdown-Container">
+        <div className="dropdown-head">
+          <h2 className="dropdown-title">{this.props.title}</h2>
+          <button onClick={this.handleBodyDropdown} className="dropdown-arrow">
+            <i
+              className={
+                this.state.openBody
+                  ? "fas fa-chevron-down up"
+                  : "fas fa-chevron-down down"
+              }
+            ></i>
+          </button>
+        </div>
+        <div
+          className={
+            this.state.openBody
+              ? "dropdown-body openBody"
+              : "dropdown-body closeBody"
+          }
+        >
+          <p>{this.props.txt}</p>
+        </div>
       </div>
-      <div className="dropdown-body">
-        <p>{props.txt}</p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default DropdownLarge
+// const DropdownLarge = (props) => {
+//   return (
+//     <div className="dropdown-Container">
+//       <div className="dropdown-head">
+//         <h2 className="dropdown-title">{props.title}</h2>
+//         <button onClick="" className="dropdown-arrow">
+//           <i className="fas fa-chevron-down"></i>
+//         </button>
+//       </div>
+//       <div className="dropdown-body">
+//         <p>{props.txt}</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default DropdownLarge
