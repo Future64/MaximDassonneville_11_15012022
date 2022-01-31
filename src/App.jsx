f1bbad8fac214d846667ec8f40157cda9d4310e6
Maxim <developement.future@gmail.com>
2022-01-31 17:34:44 +0100

    fetch dans app ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Routes, Route, useParams, useLocation } from "react-router-dom"
-import DATA from "./data/DATA"
 import dataAbout from "./data/DataAbout"
 import Home from "./Pages/Home"
 import Error from "./Pages/Error"
 import About from "./Pages/About"
 import Logement from "./Pages/Logement"
 
+let DATA = []
+fetch(" http://localhost:3000/AllData.json ", {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+})
+  .then((response) => {
+    console.log(response)
+    return response.json()
+  })
+  .then((JSONDATA) => {
+    DATA = JSONDATA
+    console.log(DATA)
+  })
+
 const App = () => {
   const params = useParams()
   const location = useLocation()
   const path = "/logement/:id"
-  console.log(location.pathname)
+  // console.log(location.pathname)
 
   // let getData = () => {
   //   DATA.map((item) => {
@@ -22,6 +37,8 @@ const App = () => {
   //     }
   //   })
   // }
+
+  console.log(DATA)
   return (
     <div className="App">
       <Routes>
