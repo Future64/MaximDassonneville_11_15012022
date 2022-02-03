368ccda30f120a515c17c2fff80d13c256ed3a98
Future64 <developement.future@gmail.com>
2022-02-03 23:29:45 +0100

    change Error src

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Header from "../components/Header"
-import "../css/error.css"
+import "../css/Error.css"
 
 const Error = () => {
   return (
