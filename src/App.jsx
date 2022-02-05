e92413ef00925714a287be2b85ff90f4e280df80
Maxim <developement.future@gmail.com>
2022-02-06 00:04:56 +0100

    tentative fetch et useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,41 @@
-import React from "react"
+import {React, useState, useEffect} from "react"
 import { Routes, Route, useLocation } from "react-router-dom"
 import dataAbout from "./data/DataAbout"
 import Home from "./pages/Home"
 import Error from "./pages/Error"
 import About from "./pages/About"
 import Logement from "./pages/Logement"
+import DATA from"./data/DATA"
 
-/**
- * Import des datas venant du fichier AllData.json
- */
-let DATA = []
-fetch(" http://localhost:3000/AllData.json ", {
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-})
-  .then((response) => {
-    return response.json()
-  })
-  .then((JSONDATA) => {
-    DATA = JSONDATA
-  })
 
-/**
- * Fonction App
- */
 const App = () => {
+
+  const [ myDATA, setDATA] = useState(DATA)
+
+  useEffect( () => {
+    fetch("http://localhost:3000/AllData.json")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setDATA(data)
+      })
+    }, [])
+
+    
   const path = "/logement/:id"
   useLocation()
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home data={DATA} />} />
-        <Route path="/about" element={<About data={dataAbout} />} />
-        <Route path={path} element={<Logement data={DATA} />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </div>
-  )
+  
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home data={myDATA} />} />
+          <Route path="/about" element={<About data={dataAbout} />} />
+          <Route path={path} element={<Logement data={myDATA} />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    ) 
 }
 
 export default App
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,30 +1,35 @@
+import {useState} from 'react'
 import "../css/Gallery.css"
 import { Link } from "react-router-dom"
 import Thumb from "./Thumb"
 
 
 const Gallery = (props) => {
-  return (
-    <>
-      <div className="Gallery">
-        {props.data.map((item) => {
-          const path = `/logement/${item.id}`
+  let[ usersData, useUsersData] = useState(props)
+  // useUsersData(usersData)
 
-          return (
-            <Link to={path} key={`${item.title}-${item.host.name}`}>
-              <Thumb
-                cover={item.cover}
-                title={item.title}
-                key={`${item.id}-${item.host.name}`}
-                data={item}
-                id={item.id}
-              />
-            </Link>
-          )
-        })}
-      </div>
-    </>
-  )
+
+    return (
+      <>
+        <div className="Gallery">
+          {usersData.data.map((item) => {
+            const path = `/logement/${item.id}`
+  
+            return (
+              <Link to={path} key={`${item.title}-${item.host.name}`}>
+                <Thumb
+                  cover={item.cover}
+                  title={item.title}
+                  key={`${item.id}-${item.host.name}`}
+                  data={item}
+                  id={item.id}
+                />
+              </Link>
+            )
+          })}
+        </div>
+      </>
+    )
 }
 
 export default Gallery
