299c65a6e0d83aedd013222d1d02642af19ef02d
Maxim <developement.future@gmail.com>
2022-02-18 14:12:59 +0100

    finission css

--- src/components/Thumb.jsx
+++ src/components/Thumb.jsx
@@ -2,13 +2,11 @@ import "../css/Thumb.css"
 
 const Thumb = (props) => {
   return (
-    <>
       <div className="Thumb" style={{ backgroundImage: `url(${props.cover})` }}>
         <div className="Thumb-filter">
           <span className="thumb-title"> {props.title} </span>
         </div>
       </div>
-    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React ,{useState, useEffect} from "react"
 import "../css/Home.css"
 import IMG from "../asset/IMG.png"
 import Header from "../components/Header"
@@ -9,18 +8,7 @@ import LogoHouse from "../asset/LogoHouse.png"
 
 const Home = (props) => {
   const sectionText = "Chez vous, partout et ailleurs"
-  // const [ myDATA, setDATA] = useState([])
 
-  // useEffect( () => {
-  //   fetch("./AllData.json")
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(data => {
-  //       console.log(data)
-  //       setDATA(data)
-  //     })
-  //   }, [])
   return (
     <div className="Home">
       <Header />
